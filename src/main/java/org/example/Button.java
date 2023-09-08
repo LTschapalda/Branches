067ec95752c29b9e210cd68129501c89c8ae3a4a
leonietschapalda <ltschapalda@googlemail.com>
2023-09-08 11:40:38 +0200

    Button method: delete

---
+++ src/main/java/org/example/Button.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Button {
+
+    public void pushButton() {
+
+        System.out.println("Du hast den Knopf gedrückt ;)");
+    }
+    public void deleteButton()  {
+        System.out.println("Du hast den Knopf gelöscht :o");
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Button button = new Button();
+        button.pushButton();
+        button.deleteButton();
+
     }
 }
