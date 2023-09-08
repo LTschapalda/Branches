1b7ea6de8b6c438a2b40d4abd1dd6195f3d9c87d
leonietschapalda <ltschapalda@googlemail.com>
2023-09-08 11:31:44 +0200

    Init

---
+++ src/main/java/org/example/Button.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Button {
+
+    public void pushButton() {
+        System.out.println("Du hast den Knopf gedrückt ;)");
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Button button = new Button();
+        button.pushButton();
+
     }
 }
